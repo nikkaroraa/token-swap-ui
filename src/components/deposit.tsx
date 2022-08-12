e184f25d07df94001a0dd0d55a7cbbd5eabc7e7b
nikhil arora <narora200@gmail.com>
2022-08-12 18:07:07 +0530

    feat: deposit liquidity

--- src/components/deposit.tsx
+++ src/components/deposit.tsx
@@ -22,10 +22,7 @@ import {
   POOL_TOKEN_MINT,
 } from "constants/programs"
 
-export const DepositSingleTokenType: FC = (props: {
-  onInputChange?: (val: number) => void
-  onMintChange?: (account: string) => void
-}) => {
+export const DepositSingleTokenType: FC = () => {
   const [poolTokenAmount, setAmount] = useState(0)
 
   const { connection } = useConnection()
@@ -40,6 +37,52 @@ export const DepositSingleTokenType: FC = (props: {
     if (!publicKey) {
       alert("Please connect your wallet!")
       return
+    }
+
+    const kryptATA = await token.getAssociatedTokenAddress(KRCYPT_TOKEN_MINT, publicKey)
+    const scroogeATA = await token.getAssociatedTokenAddress(SCROOGE_TOKEN_MINT, publicKey)
+
+    const tx = new Web3.Transaction()
+
+    const userPoolTokenATA = await token.getAssociatedTokenAddress(POOL_TOKEN_MINT, publicKey)
+    const userPoolTokenAccount = await connection.getAccountInfo(userPoolTokenATA)
+    if (userPoolTokenAccount == null) {
+      const createAtaIx = token.createAssociatedTokenAccountInstruction(
+        publicKey,
+        userPoolTokenATA,
+        publicKey,
+        POOL_TOKEN_MINT
+      )
+      tx.add(createAtaIx)
+    }
+
+    const poolTokenMint = await token.getMint(connection, POOL_TOKEN_MINT)
+
+    const depositIx = TokenSwap.depositAllTokenTypesInstruction(
+      TOKEN_SWAP_STATE_ACCOUNT,
+      SWAP_POOL_AUTHORITY,
+      publicKey,
+      kryptATA,
+      scroogeATA,
+      KRYPT_POOL_TOKEN_ACCOUNT,
+      SCROOGE_POOL_TOKEN_ACCOUNT,
+      POOL_TOKEN_MINT,
+      userPoolTokenATA,
+      TOKEN_SWAP_PROGRAM_ID,
+      token.TOKEN_PROGRAM_ID,
+      poolTokenAmount * 10 ** poolTokenMint.decimals,
+      100e9,
+      100e9
+    )
+    tx.add(depositIx)
+
+    try {
+      let txSig = await sendTransaction(tx, connection)
+      alert(`Transaction submitted: https://explorer.solana.com/tx/${txSig}?cluster=devnet`)
+      console.log(`Transaction submitted: https://explorer.solana.com/tx/${txSig}?cluster=devnet`)
+    } catch (e) {
+      console.log(JSON.stringify(e))
+      alert(JSON.stringify(e))
     }
   }
 
